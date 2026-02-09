f15d1383aa72054538f2609b1b46ec9d449adb6c
Dewin van Zaanen <dewin.vanzaanen@bettyblocks.com>
2026-02-09 16:04:58 +0100

    chore: add integration tests for split_once and split_all for split text component

---
+++ tests/functions/split-text/1.0/index.test.ts
@@ -0,0 +1,17 @@
+import { compileComponent } from "../../../compile-component.ts";
+const { test } = Deno;
+import { expect } from "@std/expect";
+
+const wasmPath = "./functions/split-text/1.0/split_text.wasm";
+
+const { splitText: { splitAll, splitOnce } } = await compileComponent(wasmPath);
+
+test("it can split all", () => {
+  const splits = splitAll("hi hi hi hi", " ");
+  expect(splits).toEqual(["hi", "hi", "hi", "hi"]);
+});
+
+test("it can split once", () => {
+  const splits = splitOnce("hi hi hi hi", " ");
+  expect(splits).toEqual(["hi", "hi hi hi"]);
+});
