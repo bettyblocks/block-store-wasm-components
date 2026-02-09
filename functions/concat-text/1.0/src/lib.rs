01bcd7e0a6bb2fb5071aa9f830d4a57a14edcbbc
Dewin van Zaanen <dewin.vanzaanen@bettyblocks.com>
2026-02-09 16:33:30 +0100

    feat: add concat_strings and concat_string_with_separator concat text wasm component functions and unit tests

--- functions/concat-text/1.0/src/lib.rs
+++ functions/concat-text/1.0/src/lib.rs
@@ -5,10 +5,36 @@ wit_bindgen::generate!({ generate_all });
 struct ConcatText;
 
 impl Guest for ConcatText {
-    fn concat_text(first_string: String, second_string: String) -> String {
-        String::default()
+    fn concat_strings(strings: Vec<String>) -> String {
+        strings.join("")
+    }
+
+    fn concat_strings_with_separator(strings: Vec<String>, separator: String) -> String {
+        strings.join(&separator)
     }
 }
 
 export! {ConcatText}
 
+#[test]
+fn can_concat_a_list_of_strings() {
+    let result = ConcatText::concat_strings(
+        vec!["hi", "hi", "hi", "hi"]
+            .into_iter()
+            .map(String::from)
+            .collect(),
+    );
+    assert_eq!(result, String::from("hihihihi"),);
+}
+
+#[test]
+fn can_concat_a_list_of_strings_with_a_separator() {
+    let result = ConcatText::concat_strings_with_separator(
+        vec!["hi", "hi", "hi", "hi"]
+            .into_iter()
+            .map(String::from)
+            .collect(),
+        String::from(" "),
+    );
+    assert_eq!(result, String::from("hi hi hi hi"),);
+}
