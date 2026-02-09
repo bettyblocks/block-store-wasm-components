b6b12d6df14b1173fd048bc241d25fa194be3fc6
Dewin van Zaanen <dewin.vanzaanen@bettyblocks.com>
2026-02-09 16:59:11 +0100

    chore: run just format

--- functions/concat-text/1.0/src/lib.rs
+++ functions/concat-text/1.0/src/lib.rs
@@ -9,7 +9,11 @@ impl Guest for ConcatText {
         format!("{}{}", first_string, second_string)
     }
 
-    fn concat_strings_with_separator(first_string: String, second_string: String, separator: String) -> String {
+    fn concat_strings_with_separator(
+        first_string: String,
+        second_string: String,
+        separator: String,
+    ) -> String {
         format!("{}{}{}", first_string, separator, second_string)
     }
 
@@ -26,10 +30,7 @@ export! {ConcatText}
 
 #[test]
 fn can_concat_two_strings() {
-    let result = ConcatText::concat_strings(
-        String::from("hi"),
-        String::from("ha"),
-    );
+    let result = ConcatText::concat_strings(String::from("hi"), String::from("ha"));
     assert_eq!(result, String::from("hiha"));
 }
 
