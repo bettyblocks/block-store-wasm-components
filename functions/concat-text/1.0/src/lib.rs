2ef56096ec249c21541ac68dc031f6ff4d2a5d6f
Dewin van Zaanen <dewin.vanzaanen@bettyblocks.com>
2026-02-09 16:49:44 +0100

    feat: add concatenating two strings for concat text with unit and integration tests

--- functions/concat-text/1.0/src/lib.rs
+++ functions/concat-text/1.0/src/lib.rs
@@ -5,11 +5,19 @@ wit_bindgen::generate!({ generate_all });
 struct ConcatText;
 
 impl Guest for ConcatText {
-    fn concat_strings(strings: Vec<String>) -> String {
+    fn concat_strings(first_string: String, second_string: String) -> String {
+        format!("{}{}", first_string, second_string)
+    }
+
+    fn concat_strings_with_separator(first_string: String, second_string: String, separator: String) -> String {
+        format!("{}{}{}", first_string, separator, second_string)
+    }
+
+    fn concat_string_list(strings: Vec<String>) -> String {
         strings.join("")
     }
 
-    fn concat_strings_with_separator(strings: Vec<String>, separator: String) -> String {
+    fn concat_string_list_with_separator(strings: Vec<String>, separator: String) -> String {
         strings.join(&separator)
     }
 }
@@ -17,8 +25,27 @@ impl Guest for ConcatText {
 export! {ConcatText}
 
 #[test]
-fn can_concat_a_list_of_strings() {
+fn can_concat_two_strings() {
     let result = ConcatText::concat_strings(
+        String::from("hi"),
+        String::from("hi"),
+    );
+    assert_eq!(result, String::from("hihi"));
+}
+
+#[test]
+fn can_concat_two_strings_with_separator() {
+    let result = ConcatText::concat_strings_with_separator(
+        String::from("hi"),
+        String::from("hi"),
+        String::from(" "),
+    );
+    assert_eq!(result, String::from("hi hi"));
+}
+
+#[test]
+fn can_concat_a_list_of_strings() {
+    let result = ConcatText::concat_string_list(
         vec!["hi", "hi", "hi", "hi"]
             .into_iter()
             .map(String::from)
@@ -29,7 +56,7 @@ fn can_concat_a_list_of_strings() {
 
 #[test]
 fn can_concat_a_list_of_strings_with_a_separator() {
-    let result = ConcatText::concat_strings_with_separator(
+    let result = ConcatText::concat_string_list_with_separator(
         vec!["hi", "hi", "hi", "hi"]
             .into_iter()
             .map(String::from)
--- tests/functions/concat-text/1.0/index.test.ts
+++ tests/functions/concat-text/1.0/index.test.ts
@@ -4,15 +4,31 @@ import { expect } from "@std/expect";
 
 const wasmPath = "./functions/concat-text/1.0/concat_text.wasm";
 
-const { concatText: { concatStrings, concatStringsWithSeparator } } =
-  await compileComponent(wasmPath);
+const {
+  concatText: {
+    concatStrings,
+    concatStringsWithSeparator,
+    concatStringList,
+    concatStringListWithSeparator,
+  },
+} = await compileComponent(wasmPath);
 
-test("it concat strings", () => {
-  const result = concatStrings(["hi", "hi", "hi", "hi"]);
+test("it concats strings", () => {
+  const result = concatStrings("hi", "hi");
+  expect(result).toEqual("hihi");
+});
+
+test("it concats strings with a separator", () => {
+  const result = concatStringsWithSeparator("hi", "hi", " ");
+  expect(result).toEqual("hi hi");
+});
+
+test("it concats string lists", () => {
+  const result = concatStringList(["hi", "hi", "hi", "hi"]);
   expect(result).toEqual("hihihihi");
 });
 
-test("it concat strings with a separator", () => {
-  const result = concatStringsWithSeparator(["hi", "hi", "hi", "hi"], " ");
+test("it concats string lists with a separator", () => {
+  const result = concatStringListWithSeparator(["hi", "hi", "hi", "hi"], " ");
   expect(result).toEqual("hi hi hi hi");
 });
