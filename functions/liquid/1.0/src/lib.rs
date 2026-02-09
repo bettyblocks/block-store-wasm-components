68ce75798ca257655f122950cc032039a34c7915
Dewin van Zaanen <dewin.vanzaanen@bettyblocks.com>
2026-02-09 10:42:51 +0100

    feat: parse template as liquid and variables as json to render in template

--- functions/liquid/1.0/src/lib.rs
+++ functions/liquid/1.0/src/lib.rs
@@ -1,16 +1,22 @@
-use crate::exports::betty_blocks::liquid::liquid::{Guest, Output};
+// TODO: Change definition here to match Crate name?
+use crate::exports::betty_blocks::liquid::liquid::{Guest};
 
 wit_bindgen::generate!({ generate_all });
 
 struct Liquid;
 
 impl Guest for Liquid {
-    fn liquid() -> Output {
-        Output {
-            placeholder: String::default()
-        }
+    fn liquid(template: String, variables: String) -> Result<String, String> {
+        // Parse the string as a JSON object to be used by the rendering.
+        let globals = liquid::to_object(&variables).map_err(|error| error.to_string())?;
+
+        let rendered_template = liquid::ParserBuilder::with_stdlib()
+            .build().map_err(|error| error.to_string())?
+            .parse(&template).map_err(|error| error.to_string())?
+            .render(&globals).map_err(|error| error.to_string())?;
+
+        Ok(rendered_template)
     }
 }
 
 export! {Liquid}
-
