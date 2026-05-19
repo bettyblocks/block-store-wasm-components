0f71b2ecd762fbb253fe88e5078559289cf1806a
Dewinz <day@gopi.day>
2026-05-19 15:57:30 +0200

    refactor: remove unnecessarily making the filename unique

--- functions/store-file-base64/1.0/src/lib.rs
+++ functions/store-file-base64/1.0/src/lib.rs
@@ -34,29 +34,18 @@ impl StoreGuest for Component {
         let content_type = mime_guess::from_path(&filename)
             .first_or_octet_stream()
             .to_string();
-        let unique_filename = make_unique_filename(&filename);
 
         let upload_result = upload_file::upload(
             &helper_context,
             &model,
             property,
             &file_bytes,
-            &unique_filename,
+            &filename,
             &content_type,
         )
         .map_err(|error| format!("Upload failed: {error}"))?;
 
         Ok(upload_result.reference)
-    }
-}
-
-fn make_unique_filename(filename: &str) -> String {
-    let random_bytes = crate::bindings::wasi::random::random::get_random_bytes(8);
-    let hex: String = random_bytes.iter().map(|b| format!("{b:02x}")).collect();
-
-    match filename.rsplit_once('.') {
-        Some((stem, ext)) => format!("{stem}_{hex}.{ext}"),
-        None => format!("{filename}_{hex}"),
     }
 }
 
