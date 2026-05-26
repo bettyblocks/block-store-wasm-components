ae65f5b29d0e3928ff6ab1d4791e9f26febe9b5d
Robin Walthuis <robinwalthuis@hotmail.com>
2026-05-26 14:04:36 +0200

    fix: replace endpoint return type with json string

--- functions/redirect/1.0/src/lib.rs
+++ functions/redirect/1.0/src/lib.rs
@@ -1,16 +1,29 @@
-use crate::exports::betty_blocks::redirect::redirect::{Guest, Output};
+use crate::exports::betty_blocks::redirect::redirect::{Guest, JsonString};
 
 wit_bindgen::generate!({ generate_all });
+
+#[derive(Debug, Clone, serde::Serialize)]
+#[cfg_attr(test, derive(serde::Deserialize))]
+struct FormattedEndpointResult {
+    #[serde(rename = "statusCode")]
+    status_code: u16,
+    body: String,
+    headers: Vec<(String, String)>,
+}
 
 struct Component;
 
 impl Guest for Component {
-    fn redirect(redirect_url: String) -> Output {
-        Output {
+    fn redirect(redirect_url: String) -> Result<JsonString, String> {
+        serde_json::to_string(&FormattedEndpointResult {
             status_code: 302,
             body: String::from("Redirect"),
-            headers: vec![(String::from("Location"), redirect_url)],
-        }
+            headers: vec![(
+                String::from("Location"),
+                redirect_url,
+            )],
+        })
+        .map_err(|_| String::from("Could not serialize redirect response as JSON"))
     }
 }
 
@@ -24,6 +37,12 @@ mod tests {
     fn redirect_points_to_correct_url() {
         let url = String::from("http://example.com");
 
-        assert_eq!(Component::redirect(url.clone()).headers[0].1, url);
+        assert_eq!(
+            serde_json::from_str::<FormattedEndpointResult>(&Component::redirect(url.clone()).unwrap())
+                .unwrap()
+                .headers[0]
+                .1,
+            url
+        );
     }
 }
