a995e5b9f9c004141acaaa05a710d46f559dd44c
Dewinz <day@gopi.day>
2026-06-05 15:31:17 +0200

    test: add tests and property tests for store-file-base64

--- functions/store-file-base64/1.0/src/lib.rs
+++ functions/store-file-base64/1.0/src/lib.rs
@@ -24,17 +24,12 @@ impl StoreGuest for Component {
         filename: String,
         file_extension: String,
     ) -> Result<String, String> {
-        if filename == String::default() {
-            return Err(String::from("Filename must be set"));
-        }
-        if file_extension == String::default() {
-            return Err(String::from("File extension must be set"));
-        }
+        check_filename_and_extension(&filename, &file_extension)?;
 
         let property = property
             .into_iter()
             .next()
-            .ok_or(String::from("Failed to fetch file property"))?;
+            .ok_or("Failed to fetch file property")?;
 
         let file_bytes = BASE64
             .decode(&data)
@@ -47,19 +42,149 @@ impl StoreGuest for Component {
             format!("{filename}.{file_extension}")
         };
 
-        let upload_result = upload_file::upload(
-            &helper_context,
-            &upload_file::Input {
-                model,
-                property,
-                file_bytes,
-                full_filename,
-            },
-        )
-        .map_err(|error| format!("Upload failed: {error}"))?;
+        if cfg!(not(test)) {
+            let upload_result = upload_file::upload(
+                &helper_context,
+                &upload_file::Input {
+                    model,
+                    property,
+                    file_bytes,
+                    full_filename,
+                },
+            )
+            .map_err(|error| format!("Upload failed: {error}"))?;
 
-        Ok(upload_result.reference)
+            Ok(upload_result.reference)
+        } else {
+            Ok(String::default())
+        }
     }
 }
 
+/// Checks if the filename and file_extension aren't empty.
+fn check_filename_and_extension(filename: &str, file_extension: &str) -> Result<(), &'static str> {
+    if filename.trim().is_empty() {
+        return Err("Filename must be set");
+    }
+    if file_extension.trim().is_empty() {
+        return Err("File extension must be set");
+    }
+
+    Ok(())
+}
+
 bindings::export!(Component with_types_in bindings);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn empty_filename_is_invalid() {
+        assert!(check_filename_and_extension("", "jpg").is_err());
+    }
+
+    #[test]
+    fn empty_file_extension_is_invalid() {
+        assert!(check_filename_and_extension("some_filename", "").is_err());
+    }
+
+    #[test]
+    fn only_whitespace_filename_is_invalid() {
+        assert!(check_filename_and_extension("   ", "jpg").is_err());
+    }
+
+    #[test]
+    fn only_whitespace_file_extension_is_invalid() {
+        assert!(check_filename_and_extension("some_filename", "   ").is_err());
+    }
+
+    #[test]
+    fn valid_filename_and_extension_is_ok() {
+        assert!(check_filename_and_extension("some_filename", "jpg").is_ok());
+    }
+
+    /// Proptests have to be run as unit tests, because integration tests on cdylib crates aren't able to directly interact with the crate.
+    mod proptests {
+        use super::*;
+        use proptest::prelude::*;
+
+
+    fn get_non_testable_params() -> (HelperContext, Model, Vec<Property>) {
+        let helper_context = HelperContext {
+            action_id: String::default(),
+            application_id: String::default(),
+            encrypted_configurations: None,
+            jwt: None,
+            log_id: String::default(),
+        };
+        let model = Model {
+            name: String::default(),
+        };
+        let property = vec![Property {
+            name: String::default(),
+        }];
+
+        (helper_context, model, property)
+    }
+
+
+        proptest! {
+            #[test]
+            fn empty_or_whitespace_filename_is_invalid(
+                filename in r"\s{0,10}",
+                file_extension in ".{1,10}",
+            ) {
+                let (helper_context, model, property) = get_non_testable_params();
+
+                let result = Component::store_file(
+                    helper_context,
+                    model,
+                    property,
+                    String::from("SGVsbG8sIFdvcmxkIGRhZmo7a2RzYWpmbGtzYWpmIQo="),
+                    filename,
+                    file_extension,
+                );
+
+                prop_assert!(result.is_err());
+            }
+
+            #[test]
+            fn empty_or_whitespace_file_extension_is_invalid(
+                filename in ".{1,10}",
+                file_extension in r"\s{0,10}",
+            ) {
+                let (helper_context, model, property) = get_non_testable_params();
+
+                let result = Component::store_file(
+                    helper_context,
+                    model,
+                    property,
+                    String::from("SGVsbG8sIFdvcmxkIGRhZmo7a2RzYWpmbGtzYWpmIQo="),
+                    filename,
+                    file_extension,
+                );
+
+                prop_assert!(result.is_err());
+            }
+
+            #[test]
+            fn non_whitespace_filename_and_extension_is_valid(
+                filename in r"\S.{0,9}",
+                file_extension in r"\S.{0,9}",
+            ) {
+                let (helper_context, model, property) = get_non_testable_params();
+
+                let result = Component::store_file(
+                    helper_context,
+                    model,
+                    property,
+                    String::from("SGVsbG8sIFdvcmxkIGRhZmo7a2RzYWpmbGtzYWpmIQo="),
+                    filename,
+                    file_extension,
+                );
+
+                prop_assert!(result.is_ok());
+            }
+        }
+    }
+}
